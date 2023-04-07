1c33604428c6c45a0f915023744b50c23eb98915
Albert <39496218+albertturon1@users.noreply.github.com>
2023-04-07 02:18:39 +0200

    created page /artworks/[id], modified way of displaying paintings on home page, modified Layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,8 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      fontFamily: {
-      },
+    letterSpacing: {
+      logo: ".5rem",
     },
   },
   plugins: [],
