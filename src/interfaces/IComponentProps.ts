4c1daa0111ac2d108c4abc1e1e042eb787017271
Albert Turon <39496218+albertturon1@users.noreply.github.com>
2023-04-10 03:56:06 +0200

    feature/fixed passing types between components; created components: CustomLink, DetailRowArrray; modified DetailRowValue; created helper function for mapping through array in DetailRowArray

--- src/interfaces/IComponentProps.ts
+++ src/interfaces/IComponentProps.ts
@@ -1,31 +1,69 @@
-import type { ArtItem } from "./IArt";
+import type { ArtItem, Images } from "./IArt";
 
 export type DetailRowTitleProps = {
-  showTitle?: true | undefined;
-  title: string
-  titleClassName?: string;
-} | {
-  showTitle: false | undefined;
+  showTitle?: boolean;
+  className?: string;
 }
-
 
 export type DetailRowValueProps = {
-  showValue?: true | undefined;
-  value: string | string[]
-  containerClassName?: string;
-} | {
-  showValue: false | undefined;
+  showValue?: boolean;
+  className?: string;
 }
 
-export type DetailRowProps = DetailRowTitleProps & DetailRowValueProps
+export type DetailRowArrayValue = {
+  value: string;
+  hrefClassName?: string;
+} & Omit<CustomLinkProps, 'className'>
+
+export type DetailRowArrayValuesProps = {
+  showValues?: boolean;
+  values: DetailRowArrayValue[];
+  hrefClassName?: string;
+} & Omit<CustomLinkProps, 'className'>
+
+
+export type DetailRowArrayProps = {
+  containerClassName?: string;
+  title: string;
+  titleClassName?: string;
+  valueClassName?: string;
+  valuesContainerClassName?: string;
+} & DetailRowTitleProps & DetailRowArrayValuesProps
 
 export type DetailRowBooleanProps = {
   showTitle?: boolean;
   showValue?: boolean;
 }
 
-export type GalleryProps = Omit<DetailRowBooleanProps, "showValue"> & {
+export type GalleryProps = {
   data: ArtItem[];
   className?: string;
-  showArtist?: boolean;
+  showCreators?: boolean;
 };
+
+export type CustomLinkProps = {
+  href?: string;
+  className?: string;
+  resetHrefStyle?: boolean
+};
+
+export type PaintingWithDetailsProps = Omit<DetailRowArrayValuesProps, "values"> &
+  Pick<DetailRowBooleanProps, "showTitle"> & {
+    item: ArtItem;
+    itemClassName?: string;
+  };
+
+export type DetailRowProps = {
+  value: string;
+  title: string;
+  containerClassName?: string;
+  titleClassName?: string;
+  valueClassName?: string;
+} & Omit<DetailRowTitleProps, 'className'> & Omit<DetailRowValueProps, 'className'>
+
+export type ArtistArtworksProps = Pick<DetailRowProps, 'containerClassName' | 'title' | 'titleClassName'> & Omit<GalleryProps, 'className'> & { galleryClassName?: string; }
+
+export type PaintingProps = {
+  images: Images;
+  className?: string;
+}
