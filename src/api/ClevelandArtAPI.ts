89b51f9fcb82e13bf0a61ce540b87d44cc80acd6
Albert <39496218+albertturon1@users.noreply.github.com>
2023-04-11 22:49:44 +0200

    fix/reverted filesize based filtration

--- src/api/ClevelandArtAPI.ts
+++ src/api/ClevelandArtAPI.ts
@@ -7,7 +7,6 @@ import type {
   ArtworksParams,
 } from "@interfaces/IArt";
 import api from "src/utils/api";
-import { artImagesData } from "src/utils/misc";
 
 export async function getArtist({
   id,
@@ -15,7 +14,7 @@ export async function getArtist({
 }: {
   id: string;
 } & ArtworksParams) {
-  const res = await api.get<ArtDataInfoResponse<Artist>, ArtErrorResponse>({
+  return await api.get<ArtDataInfoResponse<Artist>, ArtErrorResponse>({
     url: `${import.meta.env.PUBLIC_API}/creators/${id}`,
     params: {
       limit: 10,
@@ -23,23 +22,6 @@ export async function getArtist({
       ...props,
     },
   });
-
-  return {
-    ...res,
-    data: {
-      ...res.data,
-      data: {
-        ...res,
-        data: res.data?.data.artworks.filter((img) => {
-          const image = artImagesData(img.images);
-          return (
-            image &&
-            Number(image.filesize) < import.meta.env.PUBLIC_MAX_IMAGE_SIZE
-          );
-        }),
-      },
-    },
-  };
 }
 
 export async function getArtwork({ id }: { id: string | undefined }) {
@@ -50,25 +32,12 @@ export async function getArtwork({ id }: { id: string | undefined }) {
 
 export async function getArtworks({
   ...props
-}: { artists?: string | null; culture?: string | null } & ArtworksParams) {
-  const res = await api.get<ArtDataInfoResponse<ArtItem[]>, ArtErrorResponse>({
+}: { artists?: string | null; culture?: string | null; } & ArtworksParams) {
+  return await api.get<ArtDataInfoResponse<ArtItem[]>, ArtErrorResponse>({
     url: `${import.meta.env.PUBLIC_API}/artworks`,
     params: {
       has_image: 1,
       ...props,
     },
   });
-  return {
-    ...res,
-    data: {
-      ...res,
-      data: res.data?.data.filter((img) => {
-        const image = artImagesData(img.images);
-        return (
-          image &&
-          Number(image.filesize) < import.meta.env.PUBLIC_MAX_IMAGE_SIZE
-        );
-      }),
-    },
-  };
 }
